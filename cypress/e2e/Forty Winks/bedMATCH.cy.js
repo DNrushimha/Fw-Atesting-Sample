ae5d4b207504a16e72cedc364f043c40e9faef63
DNrushimha <dnrushimha@gmail.com>
2023-08-23 14:34:47 +0530

    added

---
+++ cypress/e2e/Forty Winks/bedMATCH.cy.js
@@ -0,0 +1,7 @@
+describe('bedMATCH', () => {
+    it('checking the bedMATCH Page', () => {
+        cy.visit('https://uat.fortywinks.com.au/').viewport(1280, 720);
+
+        cy.get('.gap-3 > :nth-child(4) > a').click();
+    })
+})
