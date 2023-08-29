4855ca0515deaff9622c7963f644255b9283ac47
DNrushimha <dnrushimha@gmail.com>
2023-08-29 12:45:29 +0530

    added

---
+++ cypress/e2e/Forty Winks/Bedroom Furnitures.cy.js
@@ -0,0 +1,166 @@
+describe('bedMATCH', () => {
+    it('checking the bedMATCH Page', () => {
+
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        //SHOP PRODUCT
+        //Beside tables
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/bedside-tables/').viewport(1280, 720);
+
+        //Tall Boys
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/tallboys/').viewport(1280, 720);
+
+
+        //Lowboys
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/lowboys/').viewport(1280, 720);
+
+
+        //Lingerie Chests
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/lingerie-chests/').viewport(1280, 720);
+
+
+        //Dressers
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/dressers/').viewport(1280, 720);
+
+
+        //Mirrors
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/mirrors/').viewport(1280, 720);
+
+
+        //Blanket Boxes
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/blanket-boxes/').viewport(1280, 720);
+
+
+        //Chairs
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/kids-furniture/chairs/').viewport(1280, 720);
+
+        //SHOP STYLE
+        //Glam
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/glam/').viewport(1280, 720);
+
+
+        //Urban
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/urban/').viewport(1280, 720);
+
+
+        //Scandi
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/scandi/').viewport(1280, 720);
+
+
+        //Traditional
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/traditional/').viewport(1280, 720);
+
+
+        //Coastal
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/coastal/').viewport(1280, 720);
+
+
+        //Country
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/country/').viewport(1280, 720);
+
+
+        //Kids
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/kids/').viewport(1280, 720);
+
+        //SHOP RANGE
+        //Australian Made
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/australian-made/').viewport(1280, 720);
+
+
+        //Crafted Luxe
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/crafted-luxe/').viewport(1280, 720);
+
+
+        //Modren Australia
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/modern-australia/').viewport(1280, 720);
+
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.get(':nth-child(1) > .IconTile > a > .w-full > .text-primary').click();
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.get(':nth-child(2) > .IconTile > a > .w-full > .text-primary').click();
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.get(':nth-child(3) > .IconTile > a > .w-full > .text-primary').click();
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.get(':nth-child(4) > .IconTile > a > .w-full > .text-primary').click();
+
+
+
+
+
+        //FROM OUR BLOG
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/articles-inspiration/what-happens-during-sleep/').viewport(1280, 720);
+
+
+        //FROM OUR BLOG
+
+        cy.visit('https://uat.fortywinks.com.au/bedroom-furniture/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/articles-inspiration/spinal-alignment-for-sleep/').viewport(1280, 720);
+
+
+    })
+})
--- cypress/e2e/Forty Winks/Beds.cy.js
+++ cypress/e2e/Forty Winks/Beds.cy.js
@@ -62,15 +62,90 @@ describe('bedMATCH', () => {
 
         cy.visit('https://uat.fortywinks.com.au/beds-frames/single/').viewport(1280, 720);
 
-        //
+        //Shop Style
+        //Coastal
+
         cy.visit('https://uat.fortywinks.com.au/beds-frames/').viewport(1280, 720);
-        cy.visit('').viewport(1280, 720);
-        //
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/coastal/').viewport(1280, 720);
+
+        //Urban
+
         cy.visit('https://uat.fortywinks.com.au/beds-frames/').viewport(1280, 720);
-        cy.visit('').viewport(1280, 720);
-        //
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/urban/').viewport(1280, 720);
+
+        //Traditional
+
         cy.visit('https://uat.fortywinks.com.au/beds-frames/').viewport(1280, 720);
-        cy.visit('').viewport(1280, 720);
-        // cy.visit('').viewport(1280, 720); cy.visit('').viewport(1280, 720); cy.visit('').viewport(1280, 720); cy.visit('').viewport(1280, 720); cy.visit('').viewport(1280, 720); cy.visit('').viewport(1280, 720); cy.visit('').viewport(1280, 720); cy.visit('').viewport(1280, 720); cy.visit('').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/traditional/').viewport(1280, 720);
+
+        //Scandi 
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/scandi/').viewport(1280, 720);
+
+        //Country
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/country/').viewport(1280, 720);
+
+        //Glam
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/glam/').viewport(1280, 720);
+
+        //Mix & Match
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/mix-match/').viewport(1280, 720);
+
+        //Kids
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/kids-furniture/').viewport(1280, 720);
+
+        //Shop Range
+        //Australian Made
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/australian-made/').viewport(1280, 720);
+
+        //Modren Australian Made
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/contemporary/').viewport(1280, 720);
+
+        //Crafted Luxe
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/luxury/').viewport(1280, 720);
+
+        //Timber Mix & Match
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/mix-match/').viewport(1280, 720);
+
+        //Designer upholstered
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/upholstered/').viewport(1280, 720);
+
+        //Sleep Saver Beds
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/').viewport(1280, 720);
+
+        cy.visit('https://uat.fortywinks.com.au/beds-frames/sleep-saver/').viewport(1280, 720);
     })
 })
