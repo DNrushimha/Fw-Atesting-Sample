e02c2100b7e815665a594f97483e806893b36e0b
DNrushimha <dnrushimha@gmail.com>
2023-09-22 18:12:57 +0530

    changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,21 @@
 const { defineConfig } = require("cypress");
 
+module.exports = {
+  projectId: "7zxj6f",
+  // ...rest of the Cypress project config
+}
+
 
 module.exports = defineConfig({
   e2e: {
     watchForFileChanges: false,
-    defaultCommandTimeout: 20000,
+    defaultCommandTimeout: 60000,
+
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
+
   },
 });
