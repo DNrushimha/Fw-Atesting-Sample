881effb387a8470f4cb5640454011d3a16df849f
DNrushimha <dnrushimha@gmail.com>
2023-09-29 11:34:47 +0530

    update

--- cypress/e2e/Forty Winks UAT/Stores.cy.js
+++ cypress/e2e/Forty Winks UAT/Stores.cy.js
@@ -2,11 +2,47 @@ describe('Finding store', () => {
     it('changing the Finding store near to the user', () => {
         cy.visit('https://uat.fortywinks.com.au/').viewport(1280, 720);
 
-        cy.get('.z-100 > .gap-3 > :nth-child(1) > a').click();
+        // Click the location selector
+        cy.get("header > div.flex li:nth-of-type(1) > a").click();
 
-        cy.get('.appearance-none')
-            .type('Bunbury WA, Australia')
+        // Type the postal code in the input field
+        cy.get("#__next > div.flex input").type('3000{enter}').click();
+
+        // Press Enter to submit the form
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*  cy.get('.z-100 > .gap-3 > :nth-child(1) > a')
+            .should('exist') // Ensure the link exists in the DOM
             .click();
 
-    })
-})
+        // Wait for the input field to exist and be visible
+        cy.get('.appearance-none')
+            .should('exist') // Ensure the input field exists in the DOM
+            .should('be.visible') // Ensure the input field is visible
+            .type('5000')
+            .type('{enter}'); // Press Enter key after typing the value
+
+        // Log a message to indicate the test progress
+        cy.log('Typed "5000" and pressed Enter');           */
+    });
+});
--- cypress/e2e/Forty Winks UAT/checkoutprocess.cy.js
+++ cypress/e2e/Forty Winks UAT/checkoutprocess.cy.js
@@ -4,7 +4,16 @@ describe('Check out process', () => {
 
     cy.visit('https://uat.fortywinks.com.au/products/climate-cool-colorado-plush-qb-mattress/').viewport(1280, 720);
 
-    cy.get('.font-bold.tracking-neg-0.2p').click();
+
+    cy.get("button.justify-center > span").click();
+    cy.wait(2000) // wait for 2 seconds
+
+    cy.get("div.flex > div.overflow-y-auto button").click();
+    cy.get("div.px-4 div.mt-8 button").click();
+    cy.get("div.lg\\:w-3\\/5 button").click();
+    cy.get("div.my-6 > button").click();
+    cy.get("div:nth-of-type(2) > button").click();
+    cy.get("div.lg\\:w-3\\/5 div:nth-of-type(3) > button").click();
 
 
 
